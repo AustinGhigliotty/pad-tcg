3e1e42bcc115c335a15f79f6785f6c57ca3245cc
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-10-09 17:28:51 -0500

    making deck builder

---
+++ deck-builder.js
@@ -0,0 +1,2 @@
+var savedDecks = {'deck1': ['artemis', 'evoArtemis', 'UEvoArtemis'], 'deck2': ['minerva'], 'deck3': ['shynee', 'bigShynee', 'kingShynee'],
+    'deck4': ['sapphilit'], 'deck5': ['UEvoAmaterasu', 'amaterasu', 'evoAmaterasu'], 'deck6': ['mysticMask']};
